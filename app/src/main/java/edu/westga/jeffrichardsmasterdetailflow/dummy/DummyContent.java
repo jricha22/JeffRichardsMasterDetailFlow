c7519240584735c5c1ca15bd27361a33a54585ed
Jeff Richards <jeff.richards@earthlink.net>
2016-02-13 10:43:38 -0500

    Initial commit

---
+++ app/src/main/java/edu/westga/jeffrichardsmasterdetailflow/dummy/DummyContent.java
@@ -0,0 +1,74 @@
+package edu.westga.jeffrichardsmasterdetailflow.dummy;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Helper class for providing sample content for user interfaces created by
+ * Android template wizards.
+ * <p>
+ * TODO: Replace all uses of this class before publishing your app.
+ */
+public class DummyContent {
+
+    /**
+     * An array of sample (dummy) items.
+     */
+    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+
+    /**
+     * A map of sample (dummy) items, by ID.
+     */
+    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+
+    private static final int COUNT = 25;
+
+    static {
+        // Add some sample items.
+        for (int i = 1; i <= COUNT; i++) {
+            addItem(createDummyItem(i));
+        }
+    }
+
+    private static void addItem(DummyItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+    }
+
+    private static DummyItem createDummyItem(int position) {
+        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * Website content class containing website name and url.
+     */
+    public static class DummyItem {
+        public String id;
+        public String website_name;
+        public String website_url;
+
+        public DummyItem(String id, String website_name,
+                         String website_url)
+        {
+            this.id = id;
+            this.website_name = website_name;
+            this.website_url = website_url;
+        }
+
+        @Override
+        public String toString() {
+            return website_name;
+        }
+    }
+}
